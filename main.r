0adf0437322361f88b372b4030dca7cc5a698939
alidummar <dum22003@byui.edu>
2023-10-26 13:35:07 -0500

    first commit

---
+++ main.r
@@ -0,0 +1,11 @@
+# Simple R Script
+
+# Create a vector
+numbers <- c(1, 2, 3, 4, 5)
+
+# Print the vector
+print(numbers)
+
+# Calculate the mean
+mean_value <- mean(numbers)
+print(paste("Mean:", mean_value))
